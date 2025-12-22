d83c85f29155dc7003d5f2b99c963e434237cfc1
Stephen Macke <stephen.macke@databricks.com>
2025-12-22 12:03:53 -0800

    basic extension code

--- docs/HISTORY.rst
+++ docs/HISTORY.rst
@@ -1,6 +1,10 @@
 History
 =======
 
-0.0.1 (2025-12-21)
+0.0.2 (2025-12-22)
+------------------
+* Initial semi-working release;
+
+0.0.1 (2025-12-22)
 ------------------
 * Initial placeholder release;
--- nbpipes/__init__.py
+++ nbpipes/__init__.py
@@ -3,6 +3,20 @@
 nbpipes: powerful pipeline syntax for IPython and Jupyter.
 Just run `%load_ext nbpipes` to begin using pipe operators, placeholders, and more.
 """
+from __future__ import annotations
+from IPython.core.interactiveshell import InteractiveShell
 
 from . import _version  # noqa: E402
 __version__ = _version.get_versions()['version']
+
+
+def load_ipython_extension(shell: InteractiveShell) -> None:
+	from ipyflow.shell.interactiveshell import IPyflowInteractiveShell
+
+	if not isinstance(shell, IPyflowInteractiveShell):
+		shell.run_line_magic("load_ext", "ipyflow.shell")
+	assert isinstance(shell, IPyflowInteractiveShell)
+	shell.run_line_magic("flow", "deregister dataflow")
+	shell.run_line_magic("flow", "register pyccolo.examples.PipelineTracer")
+	shell.run_line_magic("flow", "register pyccolo.examples.QuickLambdaTracer")
+
