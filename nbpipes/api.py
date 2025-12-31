8fdf96cd24d507815681aadcba543981c3b74e6d
Stephen Macke <stephen.macke@databricks.com>
2025-12-30 23:34:21 -0800

    allow variable args in various macros, enabling tuple pipe

--- nbpipes/api.py
+++ nbpipes/api.py
@@ -36,22 +36,24 @@ def peek(obj: T, *args, **kwargs) -> T:
 
 
 # Like functoolz `do`
-def do(func: Callable[[T], Any], obj: T) -> T:
-    func(obj)
-    return obj
+def do(func: Callable[[T, *tuple[T]], Any], obj: T, *extra: T) -> T | tuple[T, ...]:
+    func(obj, *extra)
+    return obj if len(extra) == 0 else (obj, *extra)
 
 
 # Call multiple functions on an input and aggregate the results into a tuple
-def fork(funcs: tuple[Callable[[T], Any]], obj: T) -> tuple[Any]:
+def fork(
+    funcs: tuple[Callable[[T, *tuple[T]], Any]], obj: T, *extra: T
+) -> tuple[Any, ...]:
     results = []
     for func in funcs:
-        results.append(func(obj))
+        results.append(func(obj, *extra))
     return tuple(results)
 
 
-def when(func: Callable[[T], bool], obj: T) -> T:
-    if func(obj):
-        return obj
+def when(func: Callable[[T, *tuple[T]], bool], obj: T, *extra: T) -> T | tuple[T, ...]:
+    if func(obj, *extra):
+        return obj if len(extra) == 0 else (obj, *extra)
     else:
         return pipeline_null  # type: ignore[return-value]
 
@@ -70,16 +72,18 @@ def collapse(results: tuple[T | None, ...]) -> T:
         return filtered_results[0]
 
 
-def future(func: Callable[[T], R], obj: T) -> Future[R]:
+def future(func: Callable[[T, *tuple[T]], R], obj: T, *extra: T) -> Future[R]:
     with ThreadPoolExecutor(max_workers=1) as executor:
-        return executor.submit(func, obj)
+        return executor.submit(func, obj, *extra)
 
 
 # Call multiple functions on an input in parallel aggregate the results into a tuple bulk-synchronously
 # Basically a parallel version of fork
-def parallel(funcs: tuple[Callable[[T], Any]], obj: T) -> tuple[Any, ...]:
+def parallel(
+    funcs: tuple[Callable[[T, *tuple[T]], Any]], obj: T, *extra: T
+) -> tuple[Any, ...]:
     futures = []
     with ThreadPoolExecutor(max_workers=len(funcs)) as executor:
         for func in funcs:
-            futures.append(executor.submit(func, obj))
+            futures.append(executor.submit(func, obj, *extra))
     return tuple(fut.result() for fut in futures)
