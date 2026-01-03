5b83617076c2871ba674d05ec3ef1a8695468379
Stephen Macke <stephen.macke@databricks.com>
2026-01-02 23:00:43 -0800

    add tests for nullish tracer

---
+++ test/test_nullish_tracer.py
@@ -0,0 +1,106 @@
+from __future__ import annotations
+
+import textwrap
+
+import pyccolo as pyc
+
+from nbpipes.nullish_tracer import NullishTracer
+from nbpipes.pipeline_tracer import PipelineTracer
+
+
+def test_optional_chaining_simple():
+    with NullishTracer:
+        pyc.exec(
+            textwrap.dedent(
+                """
+                class Foo:
+                    def __init__(self, x):
+                        self.x = x
+                foo = Foo(Foo(Foo(None)))
+                try:
+                    bar = foo.x.x.x.x
+                except:
+                    pass
+                else:
+                    assert False
+                assert foo.x.x.x?.x is None
+                assert foo.x.x.x?.x() is None
+                assert foo.x.x.x?.x?.whatever is None
+                assert isinstance(foo?.x?.x, Foo)
+                assert isinstance(foo.x?.x, Foo)
+                assert isinstance(foo?.x.x, Foo)
+                """.strip(
+                    "\n"
+                )
+            )
+        )
+
+
+def test_permissive_attr_vs_optional_attr_qualifier():
+    with NullishTracer:
+        try:
+            pyc.exec("foo = object(); assert foo?.bar is None")
+        except AttributeError:
+            pass
+        else:
+            assert False
+
+    with NullishTracer:
+        try:
+            pyc.exec("foo = object(); assert foo.?bar.baz is None")
+        except AttributeError:
+            pass
+        else:
+            assert False
+
+    with NullishTracer:
+        pyc.exec("foo = object(); assert foo.?bar is None")
+
+    with NullishTracer:
+        pyc.exec("foo = object(); assert foo.?bar?.baz is None")
+
+    with NullishTracer:
+        pyc.exec("foo = object(); assert foo.?bar?.baz.bam() is None")
+
+
+def test_call_on_optional():
+    # TODO: we are relying on PipelineTracer to populate AST bookkeeping. This should be fixed.
+    with PipelineTracer:
+        with NullishTracer:
+            pyc.exec("foo = None; assert foo?.() is None")
+
+
+def test_nullish_coalescing():
+    with NullishTracer:
+        pyc.exec("None ?? None")
+        pyc.exec("foo = ''; assert (foo ?? None) == ''")
+        pyc.exec("foo = ''; assert (foo       ?? None) == ''")
+        pyc.exec("foo = ''; assert (foo       ??       None) == ''")
+        pyc.exec("foo = ''; assert (foo ??       None) == ''")
+        assert pyc.eval("'' ?? None") == ""
+        assert pyc.eval("''??None") == ""
+        assert pyc.eval("0 ?? None") == 0
+        assert pyc.eval("None ?? 0 ?? None") == 0
+        assert pyc.eval("None or 0 ?? None") == 0
+        assert pyc.eval("None and 0 ?? None") is None
+        assert pyc.eval("0 or None ?? False") is False
+
+
+def test_multiline_nullish_coalescing():
+    with NullishTracer:
+        assert (
+            pyc.eval(
+                textwrap.dedent(
+                    """
+                (
+                    ""
+                    ??
+                    None
+                )
+                """.strip(
+                        "\n"
+                    )
+                )
+            )
+            == ""
+        )
